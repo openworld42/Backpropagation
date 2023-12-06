6b1a1da4a54056afb8e1a5a32cd4308ac5639bf7
openworld42 <github@silberbauer.xyz>
2023-12-06 17:42:01 +0100

    prepare for v1.0.1

--- src/backpropagation/BackpropNeuralNetwork.java
+++ src/backpropagation/BackpropNeuralNetwork.java
@@ -54,7 +54,7 @@ import java.util.*;
  */
 public class BackpropNeuralNetwork {
 	
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 
 	public static final float DEFAULT_LEARNING_RATE = 0.05f;
 
