f5f9e7bd761b7d9aa2f380bc66f6d6f6dbebbb55
openworld42 <github@silberbauer.xyz>
2023-12-06 21:27:21 +0100

    prepare for v1.0.1

--- src/module-info.java
+++ src/module-info.java
@@ -1,13 +1,13 @@
 /**
- * A generalized model of a backpropagation neural network to be trained and used afterwards<br/><br/>.
+ * A generalized model of a Backpropagation Neural Network to be trained and then used.<br/><br/>
  * 
  * References, algorithms and literature: <br/><br/>
  * 
  * &nbsp; &nbsp; &nbsp; R. Rojas, Neural Networks, Springer-Verlag, Berlin<br/>
  * &nbsp; &nbsp; &nbsp; David Kriesel, 2007, A Brief Introduction to Neural Networks, 
- *    <a href="www.dkriesel.com/en/science/neural_networks">www.dkriesel.com</a><br/>
+ *    <a href="https://www.dkriesel.com/en/science/neural_networks">www.dkriesel.com</a><br/>
  *    
- * @see "BackpropNeuralNetwork or one of the test programs for further information"
+ * @see "backpropagation.BackpropNeuralNetwork or one of the test programs for further information"
  */
 module Backpropagation {
 }
